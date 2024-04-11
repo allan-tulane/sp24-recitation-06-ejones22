ecddfb88d4c157ba6a034830192ec14db05a9c1d
ejones22 <143116175+ejones22@users.noreply.github.com>
2024-04-11 03:17:25 +0000

    final commit

--- answers.md
+++ answers.md
@@ -1,20 +1,26 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**___Emma Jones_____
+
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
 
 
 
-- **2)**
+- **2)** recurrence: W(n) = f(n-1) + f(n-2) +1
+  -  O(2^n)
 
-- **3)**
+- **3)** recurrence: S(n) = S(n-1) +1
+  -  O(n)
 
-- **4)**
+- **4)** count shows the computations done for each sequence, there are a lot of duplicate numbers before the final sequence is acheived.
 
-- **6)**
+- **6)** maximum n times
+  - work = O(n)
+  - span = O(n)
 
-- **8)**
+- **8)** maximum n-2 times
+  - work = O(n)
+  - span = O(n)
--- main.py
+++ main.py
@@ -6,18 +6,38 @@ def fib_recursive(n, counts):
     """    
     counts[n] += 1
     ###TODO
+    if n < 2:
+      return n
+    else:
+      return fib_recursive(n-1, counts) + fib_recursive(n-2, counts)
     pass
     
 
     
 def fib_top_down(n, fibs):
     ###TODO
+  if fibs[n] != -1:
+    return fibs[n]
+  if n < 2:
+    return n
+  else:
+    fibs[n] = fib_top_down(n-1, fibs) + fib_top_down(n-2, fibs)
+    return fibs[n]
     pass
 
 
 def fib_bottom_up(n):
     ###TODO
-    pass
+  fibs = [0] * (n+1)
+  for i in range(n+1):
+      if i < 2:
+          fibs[i] = i
+      else:
+          fibs[i] = fibs[i-1] + fibs[i-2]
+  print(fibs)
+  return fibs[-1]
+
+
 
 
 
